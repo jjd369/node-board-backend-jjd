1ab825be4e9f564a2ecc4d1e15f1a472eac9d396
jjd369 <ghana369@gmail.com>
2021-11-28 01:45:29 +0900

    init

--- src/routes/board.js
+++ src/routes/board.js
@@ -7,9 +7,7 @@ import { getBoard, getBoards, writeBoard, updateBoard, deleteBoard } from '@/ser
 const routes = Router()
 
 routes.get('/boards', authenticateToken, wrapAsync(async (req, res) => {
-
   const result = await getBoards(req.query)
-
   res.json(result).status(200)
 }))
 
@@ -19,23 +17,18 @@ routes.get('/boards/:id', authenticateToken, wrapAsync(async (req, res) => {
 }))
 
 routes.post('/write', upload.single('attachment'), authenticateToken, wrapAsync(async (req, res) => {
-
   const result = await writeBoard(req.body, req.userInfo, req.file)
-
   res.json({ message: '글 작성 완료', ...result }).status(200)
 }))
 
 routes.post('/update', upload.single('attachment'), authenticateToken, wrapAsync(async (req, res) => {
-
   const result = await updateBoard(req.body, req.userInfo, req.file)
-
   return res.json({ message: '글 수정 완료 파일과 함께', ...result })
 
 }))
 
 routes.delete('/delete', authenticateToken, wrapAsync(async (req, res) => {
   const result = await deleteBoard(req.body.id)
-
   res.json({ message: '삭제 되었습니다.', ...result }).status(200)
 }))
 
